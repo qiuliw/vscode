bf7d072ef916b1e4b525a84b3d030dd8f32b542d
邱荔枝 <1807191473@qq.com>
2024-02-25 04:53:05 +0800

    n

---
+++ cpp/jxn/24/3-3.c
@@ -0,0 +1,12 @@
+/*
+为使电文保密，将电文中字母按一定规律进行转换，非字母保持不变， 字母转换规律如下：EaA,FaB,Aaw, DaZ, eaa, fab,aaw, daz
+
+即将字母循环更改为其前第4个位置的字符（A-Z、a-z首尾相接），补全程序
+*/
+#include <stdio.h>
+#define N 4
+
+int main(){
+    
+    return 0;
+}
---
+++ cpp/jxn/24/4-3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#define N 4
+/*
+为使电文保密，将电文中字母按一定规律进行转换，非字母保持不变， 字母转换规律如下：EaA,FaB,Aaw, DaZ, eaa,fab,aaw, daz
+即将字母循环更改为其前第4个位置的字符（A-Z、a-z首尾相接），补全程序
+*/
+int main(){
+    char text[10],*p;
+    p=text;
+    gets(text);
+    while(*p){
+        if ((*p >= 'A' && *p <= 'Z') || (*p >= 'a' && *p <= 'z')) {
+            // 判断字符是否为字母
+            if (*p >= 'A' && *p <= 'Z') {
+                // 大写字母
+                *p = ((*p - 'A' + 4) % 26) + 'A';
+            } else {
+                // 小写字母
+                *p = ((*p - 'a' + 4) % 26) + 'a';
+            }
+        }
+        ++p;
+    }
+    puts(text);
+    return 0;
+}
